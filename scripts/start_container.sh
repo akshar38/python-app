f2ab6053a30174df0decab372145e0a06076cffc
akshar38 <aksharlathiya9999@gmail.com>
2024-02-09 12:00:37 +0530

    first commit

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,7 +1,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull akshar38/simple-python-flask-app
+docker pull akshar38/simple-python-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 akshar38/simple-python-flask-app
+docker run -d -p 5000:5000 akshar38/simple-python-app
